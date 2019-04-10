9708e1c10961ee4d77d7848d0abf08e98b61966a
ABADY1000 <abady1000@gmail.com>
2019-04-10 07:57:29 +0300

    Initial commit

---
+++ DES_AO.srcs/sources_1/new/DES_Main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/10/2019 07:55:38 AM
+// Design Name: 
+// Module Name: DES_Main
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DES_Main(
+
+    );
+endmodule
