8e58b1e02a9da01c386922509976cf027e560ab7
OasisArtisan <omaralama31@yahoo.com>
2019-04-16 12:45:54 +0300

    Finished writing Control module (Not tested)
    The module must be simulated and tested

--- DES_AO.srcs/sources_1/new/Control.v
+++ DES_AO.srcs/sources_1/new/Control.v
@@ -36,11 +36,11 @@ module Control #(
     assign key3       = head[(dataLengthSize+keySize*2)*8:(dataLengthSize+keySize*3)*8-1];
     assign encrypt = command == "E";
 
-    reg [0:63] dataBlockIn;
-    wire [0:63] dataBlockOut;
- 
+    reg [0:63] dataBlockIn, dataBlockOut;
+    reg pipelineClk;
+    
     TripleDES TDES(
-        .clk(clk),
+        .clk(pipelineClk),
         .reset(reset),
         .dataIn(dataBlockIn),
         .key1(key1),
@@ -61,66 +61,122 @@ module Control #(
         .packetRecievedSignal(packetRecievedSignal)
     );
     
-    reg [0:7] transmitPacket;
-    wire packetTransmittedSignal, startTransmition;
-    
-    UARTTransmiter UT(
+    wire [0:7] transmitPacket = dataBlockOut[0:7];
+    wire packetTransmittedSignal;
+    wire transmit = state == receivingTransmittingData || state == transmittingData;
+    UARTTransmitter UT(
         .clk(clk),
         .reset(reset),
-        .transmit(startTransmition),
+        .transmit(transmit),
         .data(transmitPacket),
         .tx(tx),
         .packetTransmittedSignal(packetTransmittedSignal)
     );
     // Needed counters
-    reg [0:dataLengthSize*8-1] dataPacketsCounter;
-    reg [0:9] headPacketsCounter;
-    reg [0:3] blockCounter;
-    reg [0:7] pipelineCounter;
+    reg [0:dataLengthSize*8-1] dataPacketsCounter; // Counts down from the number of data packets that we should receive
+    reg [0:9] headPacketsCounter; // Counts down from the number of packets  to be received to form the head (27 packets)
+    reg [0:3] blockCounter; // Counts the number of packets received to form a block (8 packets)
+    reg [0:6] pipelineCounter; // Counts the amount of data in the pipeline
+    reg [0:6] pipelinePushCounter; // Counts the amount of pushes we need to get data to output
+
+    always @(*) begin
+        if (pushingData) pipelineClk = clk;
+        else pipelineClk = blockCounter == 8;
+    end
     
     always @(posedge clk) begin
         if(reset) begin
             state <= waitingCommand;
             head <= 0;
             dataBlockIn <= 0;
-            transmitPacket <= 0;
-        end
-    end
-    always @(posedge packetRecievedSignal) begin
-        case(state)
-        waitingCommand: begin
-            //Shift in command in head collection
-            command <= receivedPacket;
-            state <= receivingHead;
-            headPacketsCounter <= headSize;
-        end
-        receivingHead: begin
-            // [1_dataLength,2_key1,3_key2,4_key3] Index represents sending order
-            // To achieve the above requirement we should shift left!
-            head <= {head[8:headSize*8-1],receivedPacket};
-            headPacketsCounter <= headPacketsCounter - 1;
-            // If we will finish after exiting this always block
-            if(headPacketsCounter == 1) begin
-                dataPacketsCounter <= dataLength;
-                state <= receivingData;
+            dataPacketsCounter <= 0;
+            headPacketsCounter <= 0;
+            blockCounter <= 0;
+            pipelineCounter <= 0;
+            pipelinePushCounter <= 0;
+        end else begin
+            // If we have received 8 packets (Pipeline Block)
+            if(blockCounter == 8) begin
+                blockCounter <= 0;
+            end
+            // Below is the state where receiving has stopped and we have to transmit what is left in the pipeline
+            // This means we have to operate on packetTransmittedSignal
+            if(state == transmittingData) begin
+                if(packetTransmittedSignal) begin
+                    blockCounter <= blockCounter + 1;
+                    dataBlockOut <= {dataBlockOut[8:63],8'd0};
+                    if(pipelineClk) begin
+                        pipelineCounter <= pipelineCounter - 1;
+                        if(pipelineCounter == 1) begin
+                            state <= waitingCommand;
+                        end
+                    end
+                end
+            end
+            // Below is the state where receiving has stopped and our data is not ready yet to be transmitted
+            // This means we should push it as fast as we can to be ready (No IO bottle neck)
+            // Therefore we can operate on FPGA clk
+            else if (state == pushingData) begin
+                pipelinePushCounter <= pipelinePushCounter -1;
+                if(pipelinePushCounter == 1) state <= transmittingData;
+            end
+            // Below are states that receive data
+            // This means we have to operate on packetRecievedSignal for synchronization
+            else begin
+                if(packetRecievedSignal) begin
+                    case(state)
+                        waitingCommand: begin
+                            // Shift in command in head collection
+                            command <= receivedPacket;
+                            state <= receivingHead;
+                            headPacketsCounter <= headSize;
+                        end
+                        receivingHead: begin
+                            // [1_dataLength,2_key1,3_key2,4_key3] Index represents sending order
+                            // To achieve the above requirement we should shift left!
+                            head <= {head[8:headSize*8-1],receivedPacket};
+                            headPacketsCounter <= headPacketsCounter - 1;
+                            // If we will finish after exiting this always block
+                            if(headPacketsCounter == 1) begin
+                                dataPacketsCounter <= dataLength;
+                                state <= receivingData;
+                            end
+                        end
+                        receivingData: begin
+                            // [dataBlock1, dataBlock2, dataBlock3]
+                            // To achieve the above requirement we should shift left!
+                            blockCounter <= blockCounter + 1;
+                            dataBlockIn <= {dataBlockIn[8:63],receivedPacket};
+                            dataPacketsCounter <= dataPacketsCounter - 1;
+                            // If we will finish receiving after exiting this always block
+                            if(dataPacketsCounter == 1) begin
+                                // Will the data reach the end of the pipeline after existing this always block?
+                                if(pipelineCounter == DESStages*3-1) begin
+                                    state <= transmittingData;
+                                end
+                                else begin
+                                    pipelinePushCounter <= DESStages*3-pipelineCounter;
+                                    state <= pushingData;
+                                end
+                            end
+                            else if(pipelineCounter == DESStages*3-1) begin
+                                state <= receivingTransmittingData;
+                            end
+                        end
+                        receivingTransmittingData: begin
+                            blockCounter <= blockCounter + 1;
+                            // Shift left
+                            dataBlockIn <= {dataBlockIn[8:63],receivedPacket};
+                            // Shift left
+                            dataBlockOut <= {dataBlockOut[8:63],8'd0};
+                            dataPacketsCounter <= dataPacketsCounter - 1;
+                            if(dataPacketsCounter == 1) begin
+                                state <= transmittingData;
+                            end
+                        end
+                    endcase
+                end
             end
         end
-        receivingData: begin
-            dataPacketsCounter <= dataPacketsCounter - 1;
-            // If we will finish after exiting this always block
-            if(dataPacketsCounter == 1) begin
-                state <= receivingData;
-            end
-        end
-        pushingData: begin
-            
-        end
-        receivingTransmittingData: begin
-            
-        end
-        transmittingData: begin
-            
-        end
-        endcase
     end
 endmodule
